9f6cadeb46a88f11e075e3ee03158c22b7cbb99e
Dulsara Dhanuka <dulsara.dhanuka@local.com>
2024-01-22 08:41:10 +0530

    Shakespearean text generation using transformer a based model

---
+++ model.py
@@ -0,0 +1,104 @@
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+
+class Head(nn.Module):
+    def __init__(self, block_size, n_embd, head_size, dropout):
+        super().__init__()
+        self.query = nn.Linear(n_embd, head_size, bias=False)
+        self.key = nn.Linear(n_embd, head_size, bias=False)
+        self.value = nn.Linear(n_embd, head_size, bias=False)
+        self.register_buffer('tril', torch.tril(torch.ones((block_size, block_size))))
+        self.dropout = nn.Dropout(dropout)
+    def forward(self, x):
+        _, T, C = x.shape
+        q = self.query(x)
+        k = self.key(x)
+        wei = q @ k.mT * C**-0.5
+        
+        wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf'))
+        wei = F.softmax(wei, dim=-1)
+        wei = self.dropout(wei)
+
+        v = self.value(x)
+        out = wei @ v
+        return out
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, block_size, n_embd, n_heads, dropout):
+        super().__init__()
+        self.block_size = block_size
+        self.n_embd = n_embd
+        self.n_heads = n_heads
+        assert (self.n_embd % self.n_heads) == 0
+        self.head_size = n_embd // self.n_heads
+        self.heads = nn.ModuleList([Head(self.block_size, self.n_embd, self.head_size, dropout) for _ in range(self.n_heads)])
+        self.projection = nn.Linear(n_embd, n_embd)
+        self.dropout = nn.Dropout(dropout)
+    def forward(self, x):
+        x = torch.cat([head(x) for head in self.heads], dim=-1)
+        x = self.dropout(self.projection(x))
+        return x
+
+class FeedForward(nn.Module):
+    def __init__(self, n_embd, dropout):
+        super().__init__()
+        self.n_embd = n_embd
+        self.net = nn.Sequential(
+            nn.Linear(n_embd, n_embd * 4),
+            nn.ReLU(),
+            nn.Linear(n_embd * 4, n_embd),
+            nn.Dropout(dropout),
+        )
+        
+    def forward(self, x): return self.net(x)
+
+class TransformerBlock(nn.Module):
+    def __init__(self, block_size, n_embd, n_heads, dropout):
+        super().__init__()
+        self.block_size = block_size
+        self.n_embd = n_embd
+        self.n_heads = n_heads
+        self.dropout = dropout
+
+        self.self_attention = MultiHeadAttention(self.block_size, self.n_embd, self.n_heads, self.dropout)
+        self.feedforward = FeedForward(self.n_embd, self.dropout)
+        self.ln1 = nn.LayerNorm(self.n_embd)
+        self.ln2 = nn.LayerNorm(self.n_embd)
+    def forward(self, x):
+        x = x + self.self_attention(self.ln1(x))
+        x = x + self.feedforward(self.ln2(x))
+        return x
+
+class Transformer(nn.Module):
+    def __init__(self, block_size, n_vocab, n_embd, n_heads, n_blocks, dropout, device):
+        super().__init__()
+        self.block_size = block_size
+        self.n_vocab = n_vocab
+        self.n_embd = n_embd
+        self.n_heads = n_heads
+        self.n_blocks = n_blocks
+        self.dropout = dropout
+        self.device = device
+        
+        self.embedding = nn.Embedding(self.n_vocab, self.n_embd)
+        self.positional_embedding = nn.Embedding(self.block_size, self.n_embd)
+        self.blocks = nn.Sequential(*[TransformerBlock(self.block_size, self.n_embd, self.n_heads, self.dropout) for _ in range(self.n_blocks)])
+        self.ln = nn.LayerNorm(self.n_embd)
+        self.lm_head = nn.Linear(self.n_embd, self.n_vocab)
+    def forward(self, idx, targets=None):
+        tok_embd = self.embedding(idx)
+        pos_embd = self.positional_embedding(torch.arange(0, self.block_size, device=self.device))
+        x = tok_embd + pos_embd
+        x = self.blocks(x)
+        x = self.ln(x)
+        logits = self.lm_head(x)
+        
+        if targets is None:
+            return logits, None
+        
+        B, T, C = logits.shape
+        logits = logits.view(B*T, C)
+        targets = targets.view(B*T)
+        loss = F.cross_entropy(logits, targets)
+        return logits, loss
---
+++ train.py
@@ -0,0 +1,63 @@
+import torch
+import tiktoken
+import torch.nn as nn
+from model import Transformer
+from torch.nn import functional as F
+
+enc = tiktoken.get_encoding("cl100k_base")
+with open('input.txt', 'r') as f:
+    text = f.read()
+tokens = enc.encode(text)
+n_vocab = enc.n_vocab
+
+block_size = 256
+batch_size = 64
+n_embd = 384
+n_heads = 6
+n_blocks = 6
+eval_iters = 200
+learning_rate = 3e-4
+max_iters = 5000
+eval_interval = 500
+dropout = 0.2
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+data = torch.tensor(tokens, dtype=torch.long)
+n = int(0.9*data.shape[0])
+train_data = data[:n]
+val_data = data[n:]
+
+def get_batch(split):
+    data = train_data if split == "train" else val_data
+    ix = torch.randint(len(data) - block_size, (batch_size,))
+    x = torch.stack([data[i:i+block_size] for i in ix])
+    y = torch.stack([data[i+1:i+block_size+1] for i in ix])
+    x, y = x.to(device), y.to(device)
+    return x, y
+
+@torch.no_grad()
+def estimate_loss():
+    out = {}
+    model.eval()
+    for split in ["train", "val"]:
+        losses = torch.zeros(eval_iters)
+        for k in range(eval_iters):
+            X, Y = get_batch(split)
+            _, loss = model(X, Y)
+            losses[k] = loss.item()
+        out[split] = losses.mean()
+    model.train()
+    return out
+
+model = Transformer(block_size, n_vocab, n_embd, n_heads, dropout, device)
+model.to(device)
+optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
+
+for step in range(max_iters):
+    if step % eval_interval == 0:
+        losses = estimate_loss()
+        print(f"step: {step}, training loss: {losses['train']}, validation loss: {losses['val']}")
+    logits, loss = model(*get_batch("train"))
+    optimizer.zero_grad(set_to_none=True)
+    loss.backward()
+    optimizer.step()
